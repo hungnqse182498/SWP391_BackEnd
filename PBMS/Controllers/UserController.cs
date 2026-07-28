5d066a7bd3ff17966cae7c9aa1804d30efef8cca
hungnqse182498 <hungnqse182498@fpt.edu.vn>
2026-07-28 19:36:49 +0700

    bổ sung

--- PBMS/Controllers/UserController.cs
+++ PBMS/Controllers/UserController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace PBMS.Controllers
 {
     [ApiController]
-    [Authorize(Roles = "Admin,admin")]
+    [Authorize(Roles = "Admin,Manager")]
     [Route("api/[controller]")]
     public class UserController : ControllerBase
     {
